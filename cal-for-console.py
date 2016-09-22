5bf345016efad5ab7687d97dde418d3b4fdaec3c
Delvin <delvin.fil@gmail.com>
2016-09-22 15:50:10 +0700

    Update cal-for-console.py

--- cal-for-console.py
+++ cal-for-console.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#------------------------------------
+
 #Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 #Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 #Style: DIM, NORMAL, BRIGHT, RESET_ALL
-#------------------------------------
 import datetime, time, calendar, re, locale
 from colorama import Fore, Back, Style
 locale.setlocale(locale.LC_ALL, '')
@@ -14,11 +13,11 @@ calendar.setfirstweekday(calendar.MONDAY)
 cal = calendar.month(localtime[0],localtime[1])
 #------------------------------------
 parts = cal.split('\n')
-cal = Fore.GREEN + '\n'.join(parts)+ Fore.WHITE
+cal = Fore.GREEN + '\n'.join(parts)+ Fore.LIGHTYELLOW_EX
 regex = '(?<= )%s(?= )|(?<=\n)%s(?= )|(?<= )%s(?=\n)' % (localtime[2], localtime[2], localtime[2])
-replace = Fore.WHITE + Back.RED + '%s'  %   localtime[2] + Back.RESET
-newCal = re.sub(regex, replace + Fore.WHITE, cal)
-rewek = re.sub('Сб Вс', Fore.WHITE + 'Сб Вс' + Fore.CYAN, newCal)
+replace = Fore.BLACK + Back.LIGHTRED_EX + '%s'  %   localtime[2] + Back.RESET
+newCal = re.sub(regex, replace + Fore.YELLOW, cal)
+rewek = re.sub('Сб Вс', Fore.RED + 'Сб Вс' + Fore.LIGHTBLACK_EX, newCal)
 rewek = re.sub('Пн Вт Ср Чт Пт', Fore.YELLOW + 'Пн Вт Ср Чт Пт', rewek)
 res = Style.RESET_ALL
 #------------------------------------
